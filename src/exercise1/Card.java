3d8a952e87e309704ec15671c09eee97bddcceb4
Nancy <nancy9@sheridancollege.ca>
2024-02-05 14:37:12 -0500

    activity1 1stcommit

--- src/exercise1/Card.java
+++ src/exercise1/Card.java
@@ -1,6 +1,7 @@
 package exercise1;
 
 /**
+ * 1st in class 
  * A class that models playing card Objects. Cards have 
  * a value (note that Ace = 1, Jack = 11, Queen = 12, King = 13)
  * A suit (clubs, hearts, spades, diamonds).
